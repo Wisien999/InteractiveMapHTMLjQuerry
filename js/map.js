ec983a4d3e4abaed921e5c385a2fbd24e664ac99
Wisien <wisnia3221@gmail.com>
2018-10-02 15:15:00 +0200

    JS for index.html

--- js/map.js
+++ js/map.js
@@ -6,11 +6,6 @@ $("#map").on("mouseenter", (e) => $(".cityInfo").hide());
 
 $(".city").on("click", (e) => window.location.href = "./cities/" + e.target.id + ".html");
 
-// $(".image").each(function(){
-//  var elem = $(this);
-//  var id = elem.attr('id');
-//  elem.css("background-image", "url('../img/" + id + ".jpg')");
-// });
 
-//img.css("background-image", "url('../img/" + img.attr("id") + ".jpg');");
-
+$('html, body').animate({
+    scrollTop: $(document).height()}, 500);
