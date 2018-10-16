8e64c9e33184db2a84b51053816bf6e85ee25cb0
Wisien <wisnia3221@gmail.com>
2018-10-16 18:16:13 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function setArticleHeight() {
     $(".side").each(function (index, el) {
         let it = $(this);
-        let h = it.height() + 20;
+        let h = it.height() + 40;
         it.closest(".article").css("min-height", h);
     });
 
